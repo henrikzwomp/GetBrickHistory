68feacaaea5266ec845e239bb7c232dc420fcf3d
Henrik Persson <majorcommunity@zwomp.com>
2024-04-14 09:52:25 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GetBrickHistory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var folder_with_rebrickable_db_files = @"";
+
+            Console.WriteLine("Reading Categories...");
+            var categories = RebrickableCatalogParser.ConvertPartCategoriesLinesToList(File.ReadAllLines(folder_with_rebrickable_db_files + @"\part_categories.csv"));
+            Console.WriteLine("Reading Parts...");
+            var parts = RebrickableCatalogParser.ConvertPartsLinesToList(File.ReadAllLines(folder_with_rebrickable_db_files + @"\parts.csv"));
+            Console.WriteLine("Reading Inventory Parts...");
+            var inventory_parts = RebrickableCatalogParser.ConvertInventoryPartsLinesToList(File.ReadAllLines(folder_with_rebrickable_db_files + @"\inventory_parts.csv"));
+            Console.WriteLine("Reading Inventories...");
+            var inventories = RebrickableCatalogParser.ConvertInventoryLinesToList(File.ReadAllLines(folder_with_rebrickable_db_files + @"\inventories.csv"));
+            Console.WriteLine("Reading Sets...");
+            var sets = RebrickableCatalogParser.ConvertSetLinesToSortedList(File.ReadAllLines(folder_with_rebrickable_db_files + @"\sets.csv"));
+            Console.WriteLine("Reading Elements...");
+            var elements = RebrickableCatalogParser.ConvertElementLinesToList(File.ReadAllLines(folder_with_rebrickable_db_files + @"\elements.csv"));
+            Console.WriteLine("Reading Colors...");
+            var colors = RebrickableCatalogParser.ConvertColorLinesToList(File.ReadAllLines(folder_with_rebrickable_db_files + @"\colors.csv"));
+            Console.WriteLine("Reading Inventory Minifigs...");
+            var inventory_minifigs = RebrickableCatalogParser.ConvertInventoryMinifigsLinesToList(File.ReadAllLines(folder_with_rebrickable_db_files + @"\inventory_minifigs.csv"));
+
+            var category = "Minifig Headwear";
+            var part_include_filter = new List<string>() { "hair" };
+            var part_exlude_filter = new List<string>() { "head top", "hat", "lady liberty", "bald top", "helmet", "hood", "homemaker"
+            };
+
+            var output = GetCategoryStats(category, part_include_filter, part_exlude_filter, categories, parts, inventory_parts, inventories, sets, elements, colors, inventory_minifigs);
+
+            Console.WriteLine(output);
+            File.WriteAllText("result.txt", output);
+
+            Console.Beep();
+            Console.ReadLine();
+        }
+
+        private static string GetCategoryStats(
+            string category, List<string> part_include_filter, List<string> part_exlude_filter,
+            List<RebrickablePartCategory> categories, List<RebrickablePart> parts, List<RebrickableInventoryPart> inventory_parts, List<RebrickableInventory> inventories,
+            SortedList<string, RebrickableSet> sets, List<RebrickableElement> elements, List<RebrickableColor> colors, List<InventoryMinifig> inventory_minifigs)
+        {
+            var cat_id = categories.First(x => x.Name == category).Id;
+
+            var my_list = new List<MyElementItem>();
+
+            var skip_loop = false;
+
+            var filtered_parts = parts.Where(x => x.PartCatId == cat_id);
+
+            var inv_part_count = 0;
+            var inv_part_10p = inventory_parts.Count / 10;
+            var int_part_txt = 10;
+
+            Console.WriteLine("Loop through Inventory parts list");
+            foreach (var inv_part in inventory_parts)
+            {
+                if (my_list.Any(x => x.PartNum == inv_part.PartNum && x.ColorId == inv_part.ColorId))
+                {
+                    continue;
+                }
+
+                var part = filtered_parts.FirstOrDefault(x => x.PartNum == inv_part.PartNum);
+
+                if (part == null)
+                    continue;
+
+                skip_loop = false;
+
+                foreach (var filter in part_include_filter)
+                    if (!part.Name.ToLower().Contains(filter))
+                        skip_loop = true;
+
+                foreach (var filter in part_exlude_filter)
+                    if (part.Name.ToLower().Contains(filter))
+                        skip_loop = true;
+
+                if (skip_loop)
+                    continue;
+
+                Console.Write("."); // New part & color found
+
+                my_list.Add(new MyElementItem()
+                {
+                    ElementId = -1,
+                    PartNum = part.PartNum,
+                    ColorId = inv_part.ColorId,
+                    DesignId = -1,
+                    PartName = part.PartNum,
+                });
+
+                inv_part_count++;
+                if (inv_part_count >= inv_part_10p)
+                {
+                    Console.Write(int_part_txt + "%");
+                    int_part_txt += 10;
+                    inv_part_10p += inventory_parts.Count / 10;
+                }
+            }
+            Console.Write(Environment.NewLine);
+
+            Console.WriteLine("Fill in sets and element ids");
+            foreach (var item in my_list)
+            {
+                Console.Write(".");
+
+                var element = elements.FirstOrDefault(x => x.PartNum == item.PartNum && x.ColorId == item.ColorId);
+
+                if (element != null)
+                {
+                    item.ElementId = element.ElementId;
+                    item.DesignId = element.DesignId;
+                }
+
+                item.Sets = new List<RebrickableSet>();
+
+                foreach (var inventory_part in inventory_parts.Where(x => x.PartNum == item.PartNum && x.ColorId == item.ColorId))
+                {
+                    var set_mf_ids = inventories.Where(x => x.Id == inventory_part.InventoryId).First().SetNum;
+
+                    if (sets.ContainsKey(set_mf_ids)) // Is in set directly?
+                    {
+                        item.Sets.Add(sets[set_mf_ids]);
+                    }
+                    else if (inventory_minifigs.Any(x => x.FigNum == set_mf_ids)) // Is in set via minifig
+                    {
+                        foreach (var inventory_id in inventory_minifigs.Where(x => x.FigNum == set_mf_ids).Select(y => y.InventoryId))
+                        {
+                            var set_id = inventories.Where(x => x.Id == inventory_id).First().SetNum;
+
+                            if (!item.Sets.Any(x => x.SetNum == set_id)) // Check if already exists
+                                item.Sets.Add(sets[set_id]);
+                        }
+                    }
+
+                    item.ImgUrl = inventory_part.ImgUrl;
+                }
+
+                if (item.Sets.Count > 0)
+                {
+                    item.IntroYear = item.Sets.OrderBy(x => x.Year).First().Year;
+                }
+            }
+            Console.Write(Environment.NewLine);
+
+            Console.WriteLine("Done!");
+
+            // Print out by color
+            var output = new StringBuilder();
+            var set_count = 0;
+
+            foreach (var color in colors.OrderBy(x => x.Name))
+            {
+                if (color.Id == 191)
+                {
+                    Console.Beep();
+                    Console.Write("Blipp");
+                }
+
+                if (!my_list.Any(x => x.ColorId == color.Id && x.Sets.Any()))
+                    continue;
+
+                var parts_in_current_color = my_list.Where(x => x.ColorId == color.Id && x.Sets.Any()).ToList(); // current color and is in a set
+
+                set_count = parts_in_current_color.Select(x => x.Sets.Count).Sum();
+
+                var intro_year = parts_in_current_color.OrderBy(y => y.IntroYear).First().IntroYear;
+
+                var first_in_color = parts_in_current_color.Where(x => x.IntroYear == intro_year).OrderBy(y => y.ElementId);
+
+                output.AppendLine(color.Name + "\t" +
+                    intro_year + "\t" +
+                    parts_in_current_color.Count + "\t" +
+                    set_count + "\t" +
+                    String.Join(", ", first_in_color.Select(x => x.ElementId)) + "\t" +
+                    String.Join(", ", first_in_color.Select(x => x.DesignId)) + "\t" +
+                    String.Join(", ", first_in_color.Select(x => x.PartNum)));
+            }
+
+            return output.ToString();
+        }
+
+        class MyElementItem
+        {
+            public int ElementId { get; set; }
+            public string PartNum { get; set; }
+            public int ColorId { get; set; }
+            public string ColorName { get; set; } // ToDo
+            public int? DesignId { get; set; }
+            public string PartName { get; set; }
+            public List<RebrickableSet> Sets { get; set; }
+            public int IntroYear { get; set; }
+            public string ImgUrl { get; set; }
+        }
+    }
+}
---
+++ RebrickableCatalogParser.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetBrickHistory
+{
+    public class RebrickableCatalogParser
+    {
+        public static List<RebrickablePartCategory> ConvertPartCategoriesLinesToList(string[] file_lines)
+        {
+            return GetList<RebrickablePartCategory>(file_lines);
+        }
+
+        public static List<RebrickablePart> ConvertPartsLinesToList(string[] file_lines)
+        {
+            return GetList<RebrickablePart>(file_lines);
+        }
+        public static List<RebrickableInventoryPart> ConvertInventoryPartsLinesToList(string[] file_lines)
+        {
+            return GetList<RebrickableInventoryPart>(file_lines);
+        }
+
+        public static List<RebrickableInventory> ConvertInventoryLinesToList(string[] file_lines)
+        {
+            return GetList<RebrickableInventory>(file_lines);
+        }
+
+        public static SortedList<string, RebrickableSet> ConvertSetLinesToSortedList(string[] file_lines)
+        {
+            return GetSortedList<RebrickableSet>(file_lines, "SetNum");
+        }
+
+        public static List<RebrickableElement> ConvertElementLinesToList(string[] file_lines)
+        {
+            return GetList<RebrickableElement>(file_lines);
+        }
+
+        public static List<RebrickableColor> ConvertSetLinesToList(string[] file_lines)
+        {
+            return GetList<RebrickableColor>(file_lines);
+        }
+
+        public static List<RebrickableColor> ConvertColorLinesToList(string[] file_lines)
+        {
+            return GetList<RebrickableColor>(file_lines);
+        }
+
+        // ConvertInventoryMinifigsLinesToList
+        public static List<InventoryMinifig> ConvertInventoryMinifigsLinesToList(string[] file_lines)
+        {
+            return GetList<InventoryMinifig>(file_lines);
+        }
+
+        private static List<LineType> GetList<LineType>(string[] file_lines) where LineType : new()
+        {
+            if (!VerifyHeaderLineFor<LineType>(file_lines[0]))
+                throw new Exception("First line with headers different than expected.");
+
+            var baselist = ConvertLinesTo<LineType>(
+                ProcessLines<LineType>(file_lines.Skip(1)));
+
+            return baselist;
+        }
+
+        private static SortedList<string, LineType> GetSortedList<LineType>(string[] file_lines, string key_column) where LineType : new()
+        {
+            var baselist = GetList<LineType>(file_lines);
+
+            var result = new SortedList<string, LineType>();
+
+            var key_property = typeof(LineType).GetProperties().First(x => x.Name == key_column);
+
+            foreach (var line in baselist)
+                result.Add(key_property.GetValue(line).ToString(), line);
+
+            return result;
+        }
+
+        private static List<line_class> ConvertLinesTo<line_class>(IEnumerable<string[]> parsed_lines) where line_class : new()
+        {
+            var class_properties = typeof(line_class).GetProperties();
+
+            var result = new List<line_class>();
+
+            foreach (var line in parsed_lines)
+            {
+                var new_item = new line_class();
+
+                for (int i = 0; i < class_properties.Length; i++)
+                {
+                    if (class_properties[i].PropertyType == typeof(string))
+                    {
+                        class_properties[i].SetValue(new_item, line[i]);
+                    }
+                    else if (class_properties[i].PropertyType == typeof(int))
+                    {
+                        class_properties[i].SetValue(new_item, int.Parse(line[i]));
+                    }
+                    else if (class_properties[i].PropertyType == typeof(int?))
+                    {
+                        if (line[i] == "")
+                            class_properties[i].SetValue(new_item, null);
+                        else
+                            class_properties[i].SetValue(new_item, int.Parse(line[i]));
+                    }
+                    else if (class_properties[i].PropertyType == typeof(bool))
+                    {
+                        var bool_value = false;
+
+                        if (line[i].ToLower() == "t")
+                            bool_value = true;
+
+                        class_properties[i].SetValue(new_item, bool_value);
+                    }
+                    else
+                    {
+                        throw new Exception("PropertyType not supported");
+                    }
+                }
+
+                result.Add(new_item);
+            }
+
+            return result;
+        }
+
+        private static List<string[]> ProcessLines<line_class>(IEnumerable<string> lines)
+        {
+            var separator = ',';
+
+            var result = new List<string[]>();
+
+            foreach (string line in lines)
+            {
+                if (line.Contains("\""))
+                {
+                    var quote_on = false;
+                    var part = "";
+                    var parts = new List<string>();
+
+                    foreach (char linechar in line)
+                    {
+                        if (linechar == '"')
+                        {
+                            quote_on = !quote_on;
+                        }
+                        else if (linechar == separator)
+                        {
+                            if (quote_on == false)
+                            {
+                                parts.Add(part);
+                                part = "";
+                            }
+                            else
+                            {
+                                part += linechar;
+                            }
+                        }
+                        else
+                        {
+                            part += linechar;
+                        }
+
+                    }
+                    parts.Add(part);
+
+                    result.Add(parts.ToArray());
+                }
+                else
+                {
+                    result.Add(line.Split(separator));
+                }
+            }
+            return result;
+        }
+
+        private static bool VerifyHeaderLineFor<line_class>(string header_line)
+        {
+            var separator = ',';
+
+            header_line = header_line.Replace(" ", "");
+            header_line = header_line.Replace("_", "");
+
+            var input_headers = header_line.Split(separator);
+
+            var class_properties = typeof(line_class).GetProperties();
+
+            if (input_headers.Length != class_properties.Length)
+                return false;
+
+            for (int i = 0; i < class_properties.Length; i++)
+            {
+                if (class_properties[i].Name.ToLower() != input_headers[i].ToLower())
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
+    public class RebrickablePartCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class RebrickablePart
+    {
+        public string PartNum { get; set; }
+        public string Name { get; set; }
+        public int PartCatId { get; set; }
+        public string PartMaterial { get; set; }
+    }
+
+    public class RebrickableInventoryPart
+    {
+        public int InventoryId { get; set; }
+        public string PartNum { get; set; }
+        public int ColorId { get; set; }
+        public int Quantity { get; set; }
+        public bool IsSpare { get; set; }
+        public string ImgUrl { get; set; }
+    }
+
+    public class RebrickableInventory
+    {
+        public int Id { get; set; }
+        public int Version { get; set; }
+        public string SetNum { get; set; }
+    }
+
+    public class RebrickableSet
+    {
+        public string SetNum { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int ThemeId { get; set; }
+        public int NumParts { get; set; }
+        public string ImgUrl { get; set; }
+    }
+
+    public class RebrickableElement
+    {
+        public int ElementId { get; set; }
+        public string PartNum { get; set; }
+        public int ColorId { get; set; }
+        public int? DesignId { get; set; }
+    }
+
+    public class RebrickableColor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string RGB { get; set; }
+        public bool IsTrans { get; set; }
+    }
+
+    public class InventoryMinifig
+    {
+        public int InventoryId { get; set; }
+        public string FigNum { get; set; }
+        public int Quantity { get; set; }
+    }
+}
